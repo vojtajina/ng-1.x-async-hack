68ccb7dd71a698212f7670933e1074dbd7b74b51
doodeec <doodeec@gmail.com>
2014-03-14 08:59:01 +0100

    added directive/factory/filter/value lazy providers

--- js/lazy-list.js
+++ js/lazy-list.js
@@ -2,12 +2,28 @@
 
 var list = angular.module('call me whatever', []);
 
-list.service('foo', function() {
-  this.name = 'foo';
+list.service('foo', function () {
+    this.name = 'foo';
 });
 
-list.controller('List', function($scope, foo) {
-  $scope.alert = function() {
-    alert(foo.name);
-  };
+list.controller('List', function ($scope, foo, lazyService) {
+    $scope.alert = function () {
+        alert(foo.name);
+        alert(lazyService.first);
+    };
+});
+
+list.directive('lazyList', function () {
+    return {
+        replace: true,
+        restrict: 'EA',
+        template: '<div>Lazy loaded directive</div>'
+    }
+});
+
+list.factory('lazyService', function () {
+    return {
+        first: 'First Value',
+        second: 'Second Value'
+    }
 });
